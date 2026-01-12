d542bc0aef86a788eaa2a3983993d83aa121c40d
John Gerrard Holland <john_holland1@brown.edu>
2026-01-12 16:57:07 +0000

    add basic test setup

--- src/CLAHE.jl
+++ src/CLAHE.jl
@@ -1,5 +1,55 @@
 module CLAHE
 
+# Boilerplate from ImageContrastAdjustment.jl
+using ImageContrastAdjustment
+using ImageContrastAdjustment: AbstractHistogramAdjustmentAlgorithm, GenericGrayImage, imresize
+using ImageCore
+using ImageBase
+
+# Where possible we avoid a direct dependency to reduce the number of [compat] bounds
+using ImageCore.MappedArrays
+using Parameters: @with_kw # Same as Base.@kwdef but works on Julia 1.0
+
 greet() = print("Hello World!")
+
+"""
+```
+    ContrastLimitedAdaptiveEqualization <: AbstractHistogramAdjustmentAlgorithm
+    ContrastLimitedAdaptiveEqualization(; nbins = 128, minval = 0, maxval = 1, rblocks = 8, cblocks = 8, clip = 0.1)
+
+    adjust_histogram([T,] img, f::ContrastLimitedAdaptiveEqualization)
+    adjust_histogram!([out,] img, f::ContrastLimitedAdaptiveEqualization)
+```
+
+Performs Contrast Limited Adaptive Histogram Equalisation (CLAHE) on the input
+image. 
+
+This version is based on the description in:
+GraphicsGems IV, "Contrast Limited Adaptive Histogram Equalization".
+
+"""
+@with_kw struct ContrastLimitedAdaptiveEqualization{T₁<:Union{Real,AbstractGray},
+    T₂<:Union{Real,AbstractGray},
+    T₃<:Real} <: AbstractHistogramAdjustmentAlgorithm
+    nbins::Int = 128
+    minval::T₁ = 0.0
+    maxval::T₂ = 1.0
+    rblocks::Int = 8
+    cblocks::Int = 8
+    clip::T₃ = 0.1
+
+end
+
+function (f::ContrastLimitedAdaptiveEqualization)(out::GenericGrayImage, img::GenericGrayImage)
+    out .= img # Placeholder
+    return out
+end
+
+function validate_parameters(f::ContrastLimitedAdaptiveEqualization)
+    !(0 <= f.clip <= 1) && throw(ArgumentError("The parameter `clip` must be in the range [0..1]."))
+    !(1 <= f.rblocks && 1 <= f.cblocks) && throw(ArgumentError("The parameters `rblocks` and `cblocks` must be greater than 0."))
+end
+
+export ContrastLimitedAdaptiveEqualization, adjust_histogram, adjust_histogram!
 
 end # module CLAHE
---
+++ test/runtests.jl
@@ -0,0 +1,30 @@
+using Test
+
+@testitem "Initialization tests" begin
+    @test ContrastLimitedAdaptiveEqualization() isa ContrastLimitedAdaptiveEqualization
+end
+
+
+@testitem "Parameter validation tests" begin
+    using CLAHE: validate_parameters
+    @test_throws ArgumentError validate_parameters(ContrastLimitedAdaptiveEqualization(; clip=-0.1))
+    @test_throws ArgumentError validate_parameters(ContrastLimitedAdaptiveEqualization(; clip=1.1))
+    @test_throws ArgumentError validate_parameters(ContrastLimitedAdaptiveEqualization(; rblocks=0))
+    @test_throws ArgumentError validate_parameters(ContrastLimitedAdaptiveEqualization(; rblocks=-1))
+    @test_throws ArgumentError validate_parameters(ContrastLimitedAdaptiveEqualization(; cblocks=0))
+    @test_throws ArgumentError validate_parameters(ContrastLimitedAdaptiveEqualization(; cblocks=-1))
+end
+
+
+@testitem "Null operation tests" begin
+    using CLAHE: ContrastLimitedAdaptiveEqualization, adjust_histogram
+    using ImageCore: Gray, fill, n0f8
+    img = fill(Gray(n0f8(0.5)), 16, 16)
+    f = ContrastLimitedAdaptiveEqualization(; clip=1.0, rblocks=1, cblocks=1)
+    out = adjust_histogram(img, f)
+    @info "input image:"
+    @info img
+    @info "Output image:"
+    @info out
+    @test all(out .== img)
+end
