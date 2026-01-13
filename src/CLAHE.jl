b0a68ecc9aa4d26fccfdcb92d14fdd3d7e294529
John Gerrard Holland <john_holland1@brown.edu>
2026-01-13 15:52:06 +0000

    add basic interpolation

--- src/CLAHE.jl
+++ src/CLAHE.jl
@@ -80,25 +80,91 @@ function (f::ContrastLimitedAdaptiveEqualization)(out::GenericGrayImage, img::Ge
         region = view(img_tmp, rstart:rend, cstart:cend)
         edges, raw_counts = build_histogram(region, f.nbins, minval=f.minval, maxval=f.maxval)
         redistributed_counts = redistribute_histogram(raw_counts, clip_limit)
-        mapped_values = map_histogram(redistributed_counts, f.minval, f.maxval)
+        mapping_function = map_histogram(edges::AbstractArray, redistributed_counts, f.minval, f.maxval)
         # @info "Lengths: edges=$(length(edges)), raw_counts=$(length(raw_counts)), redistributed_counts=$(length(redistributed_counts)), mapped_values=$(length(mapped_values))"
-        histograms[rblock, cblock] = (edges, mapped_values)
+        histograms[rblock, cblock] = mapping_function
         # @info "edges: $(Array(edges)), raw_counts: $raw_counts, redistributed_counts: $redistributed_counts, mapped_values: $mapped_values"
     end
 
     # Interpolate pixel values
-    for rblock in 1:(f.rblocks), cblock in 1:(f.cblocks)
-        # histLU, histRU, histLB, histRB
+    for rblock in 1:f.rblocks+1, cblock in 1:f.cblocks+1 # use zero-indexing here because we're not in the original format
 
-        # Naive implementation – no interpolation
-        rstart = Int((rblock - 1) * rsize) + 1
-        rend = Int(rblock * rsize)
-        cstart = Int((cblock - 1) * csize) + 1
-        cend = Int(cblock * csize)
+        if rblock == 1
+            idUr, idBr = 1, 1
+        elseif rblock == f.rblocks + 1
+            idUr, idBr = f.rblocks, f.rblocks
+        else
+            idUr, idBr = rblock - 1, rblock
+        end
+
+        if cblock == 1
+            idLc, idRc = 1, 1
+        elseif cblock == f.cblocks + 1
+            idLc, idRc = f.cblocks, f.cblocks
+        else
+            idLc, idRc = cblock - 1, cblock
+        end
+
+        if rblock ∈ [1, f.rblocks + 1]
+            rblockpix = rsize / 2
+        else
+            rblockpix = rsize
+        end
+
+        if rblock == 1
+            rblockoffset = 0
+        else
+            rblockoffset = -(rsize / 2)
+        end
+
+        if cblock ∈ [1, f.cblocks + 1]
+            cblockpix = csize / 2
+        else
+            cblockpix = csize
+        end
+
+        if cblock == 1
+            cblockoffset = 0
+        else
+            cblockoffset = -(csize / 2)
+        end
+
+        histUL, histUR = histograms[idUr, idLc], histograms[idUr, idRc]
+        histBL, histBR = histograms[idBr, idLc], histograms[idBr, idRc]
+
+        rstart = Int((rblock - 1) * rsize + rblockoffset) + 1
+        rend = Int(rstart + rblockpix) - 1
+
+        cstart = Int((cblock - 1) * csize + cblockoffset) + 1
+        cend = Int(cstart + cblockpix) - 1
+
+
+        @info "r$rblock, c$cblock:"
+        @info "[$idUr, $idLc], [$idUr, $idRc]"
+        @info "[$idBr, $idLc], [$idBr, $idRc]"
+        @info "$rstart:$rend, $cstart:$cend"
+
         region = view(img_tmp, rstart:rend, cstart:cend)
         out_region = view(out_tmp, rstart:rend, cstart:cend)
-        edges, mapped_values = histograms[rblock, cblock]
-        out_region .= remap_value.(region; edges, mapped_values)
+
+        resultUL, resultUR = histUL.(region), histUR.(region)
+        resultBL, resultBR = histBL.(region), histBR.(region)
+
+        x₁, x₂ = rstart, rend
+        y₁, y₂ = cstart, cend
+        x = Array(range(rstart, rend))
+        y = Array(range(cstart, cend))'
+
+        w₁₁ = ((x₂ .- x) .* (y₂ .- y)) / ((x₂ - x₁) * (y₂ - y₁))
+        w₁₂ = ((x₂ .- x) .* (y .- y₁)) / ((x₂ - x₁) * (y₂ - y₁))
+        w₂₁ = ((x .- x₁) .* (y₂ .- y)) / ((x₂ - x₁) * (y₂ - y₁))
+        w₂₂ = ((x .- x₁) .* (y .- y₁)) / ((x₂ - x₁) * (y₂ - y₁))
+
+
+        # @info "size(w₁₁): $(size(w₁₁)), size(resultUL): $(size(resultUL))"
+        # @show w₁₁ .* resultUL + w₁₂ .* resultUR + w₂₁ .* resultBL + w₂₂ .* resultBR
+
+        @. out_region = w₁₁ * resultUL + w₁₂ * resultUR + w₂₁ * resultBL + w₂₂ * resultBR
     end
 
     out .= must_resize ? imresize(out_tmp, (height, width)) : out_tmp
@@ -107,7 +173,7 @@ end
 
 function validate_parameters(f::ContrastLimitedAdaptiveEqualization)
     !(0 <= f.clip <= 1) && throw(ArgumentError("The parameter `clip` must be in the range [0..1]."))
-    !(2 <= f.rblocks && 2 <= f.cblocks) && throw(ArgumentError("At least 4 contextual regions required (2x2 or greater)."))
+    !(1 <= f.rblocks && 1 <= f.cblocks) && throw(ArgumentError("At least 1 contextual regions required (1x1 or greater)."))
 end
 
 function redistribute_histogram(counts::AbstractVector{T}, clip_limit::Int) where T<:Integer
@@ -127,7 +193,7 @@ function redistribute_histogram(counts::AbstractVector{T}, clip_limit::Int) wher
     return new_counts
 end
 
-function map_histogram(counts::AbstractVector{T}, minval::Union{Real,AbstractGray}, maxval::Union{Real,AbstractGray}) where T<:Integer
+function map_histogram(edges::AbstractArray, counts::AbstractVector{T}, minval::Union{Real,AbstractGray}, maxval::Union{Real,AbstractGray}) where T<:Integer
     n_pixels = sum(counts)
     scale = (maxval - minval) / n_pixels
     mapping = similar(counts, Float64) # Can I make this the same type as minval/maxval?
@@ -139,13 +205,14 @@ function map_histogram(counts::AbstractVector{T}, minval::Union{Real,AbstractGra
             mapping[i] = maxval
         end
     end
-    return mapping
+
+    function _mapping_function(value)
+        index = searchsortedfirst(edges, value)
+        return mapping[index-1] # -1 because mapped_values is an OffsetArray
+    end
+    return _mapping_function
 end
 
-function remap_value(value; edges, mapped_values)
-    index = searchsortedfirst(edges, value)
-    return mapped_values[index-1] # -1 because mapped_values is an OffsetArray
-end
 
 export ContrastLimitedAdaptiveEqualization, adjust_histogram, adjust_histogram!
 
